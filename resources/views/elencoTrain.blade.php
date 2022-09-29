4d73117b616c01f92603b4b5d0aaf4087274c756
pacimarco <m.paci1987@gmail.com>
2022-09-29 19:04:58 +0200

    prove stampa lista treni

---
+++ resources/views/elencoTrain.blade.php
@@ -0,0 +1,8 @@
+<div>
+    @foreach ($train as $train)
+    <ul>
+        <li>{{$trainList['agency']}}</li>
+    </ul>
+        
+    @endforeach
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $elenco = config('train');
+    return view('elencoTrain');
 });

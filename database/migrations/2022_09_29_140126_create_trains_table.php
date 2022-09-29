1be2df0107e46ff66e187a9235259d1fc2ab0604
pacimarco <m.paci1987@gmail.com>
2022-09-29 17:08:07 +0200

    registrazione db e migrazione nuove table

--- database/migrations/2022_09_29_140126_create_trains_table.php
+++ database/migrations/2022_09_29_140126_create_trains_table.php
@@ -16,8 +16,16 @@ class CreateTrainsTable extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('agency','100');
+            $table->date('date');
             $table->string('departureStation','100');
             $table->string('arrivalStation','100');
+            $table->time('departureTime', $precision = 0);
+            $table->time('arrivalTime', $precision = 0);
+            $table->integer('trainCode');
+            $table->string('numOfCarriage','4');
+            $table->string('inTime','10');
+            $table->string('deleted','10');
+
             $table->timestamps();
         });
     }

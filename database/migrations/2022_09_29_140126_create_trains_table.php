06763451dd5372fe8f0035e853949a70b88116f7
pacimarco <m.paci1987@gmail.com>
2022-09-29 16:19:58 +0200

    struttura base + migrazione trains

--- database/migrations/2022_09_29_140126_create_trains_table.php
+++ database/migrations/2022_09_29_140126_create_trains_table.php
@@ -15,6 +15,9 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency','100');
+            $table->string('departureStation','100');
+            $table->string('arrivalStation','100');
             $table->timestamps();
         });
     }
